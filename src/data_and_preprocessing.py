99f4e5b19eb4683ed6eb57729b8ba1559c734ea6
beltobel <125828207+beltobel@users.noreply.github.com>
2024-12-18 21:24:08 +0300

    Update data_and_preprocessing.py

--- src/data_and_preprocessing.py
+++ src/data_and_preprocessing.py
@@ -8,18 +8,18 @@ def preprocess_data(df):
     df['headline_length'] = df['headline'].apply(len)
     # df['date'] = pd.to_datetime(df['date'])
     # df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
-    # df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
     #Clean the date column
-    df['date'] = df['date'].str.strip()
-    def parse_dates(date_str):
-        for fmt in ("%Y-%m-%d %H:%M:%S", "%Y/%m/%d", "%d-%m-%Y"):
-            try:
-                return pd.to_datetime(date_str, format=fmt)
-            except ValueError:
-                continue
-            return pd.NaT
+    # df['date'] = df['date'].str.strip()
+    # def parse_dates(date_str):
+    #     for fmt in ("%Y-%m-%d %H:%M:%S", "%Y/%m/%d", "%d-%m-%Y"):
+    #         try:
+    #             return pd.to_datetime(date_str, format=fmt)
+    #         except ValueError:
+    #             continue
+    #         return pd.NaT
 
 
-    df['date'] = df['date'].apply(parse_dates)
+    # df['date'] = df['date'].apply(parse_dates)
 
     return df
